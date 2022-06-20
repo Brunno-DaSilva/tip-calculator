921bb119db30516d136e70401c842211055c8d15
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-20 17:07:16 -0500

    pushing initial getuser state

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInput from "../UserInput/UserInput";
 import Result from "../Result/Result";
 
 import "./Calculator.css";
 
 const Calculator = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="main_calculator">
-      <UserInput />
+      <UserInput handleChange={handleChange} />
       <Result />
     </div>
   );
