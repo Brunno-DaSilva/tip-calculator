7bf92b34212608023fbb74374b448cfd4406840b
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-23 15:51:52 -0500

    FINISHED: Responsive design

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <div className="gird"></div>
-      <div className="gird"></div>
+      <div className="grid"></div>
+      <div className="grid-mb"></div>
       <header className="app__header">
         <h1 className="header__title">
           <span>SPLI</span>
           <span>TTER</span>
         </h1>
       </header>
+      <div className="grid-mb"></div>
+      <div className="grid"></div>
       <Calculator />
-      <div className="gird"></div>
+      <div className="grid"></div>
     </div>
   );
 }
